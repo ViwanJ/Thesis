999e9767c3d2f0ef881b04fa72b04d7486f93ff2
Viwan Jarerattanachat <viwan.jrrt@gmail.com>
2018-06-24 16:23:55 +0100

    updated lipid contact analysis

--- Ch6-NB_FDA/Other_tools/Lipid_contact_VJ.py
+++ Ch6-NB_FDA/Other_tools/Lipid_contact_VJ.py
@@ -17,6 +17,7 @@ parser.add_argument('-r', '--r_cutoff', help='distance cutoff in Angstrom', defa
 parser.add_argument('-b', help='First frame to read from trajectory', default = 0, nargs='?')
 parser.add_argument('-e', help='Last frame to read from trajectory', default = -1, nargs='?')
 parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+
 args = parser.parse_args()
 print "\n\n\n"
 print args, "\n\n"
@@ -43,7 +44,8 @@ print "time to setup " + str(check1 - start )
 ######################################################
 # Run on each new frame:
 ### set time step parameter
-Contact_t = []
+ContactC_t = []
+ContactP_t = []
 ### strat loop
 if int(args.e) == -1 :
 	args.e = len(u.trajectory)
@@ -57,23 +59,33 @@ for ts in u.trajectory[int(args.b):args.e] :
 		print ts
 ### Lood over all atoms ###
 	check4 = timeit.default_timer()
-	num_lip_around = np.zeros(len(Protein.residues.resids))
+	num_C_around, num_P_around  = np.zeros(len(Protein.residues.resids)), np.zeros(len(Protein.residues.resids))
 	for k in Protein.residues.resids : ##change from index
 		a_lip = u.select_atoms("not protein" + " and around " + str(r_cutoff) + " resid " + str(k))
-		num_lip_around[k-1] = len(a_lip.residues.resids)
+		foo = [name[0] for name in a_lip.names if name not in ['C1', 'C2', 'C3', 'C11','C12', 'C13', 'C14', 'C15']] ## do not count fist C of each chains and C1*
+		num_C_around[k-1] = foo.count('C')
+		num_P_around[k-1] = foo.count('P')
 	check2 = timeit.default_timer()
 	if args.verbose :
 		print "time to loop all residues " + str(check2 - check4 )
-	Contact_t.append(num_lip_around)
+	ContactC_t.append(num_C_around)
+	ContactP_t.append(num_P_around)
 
-Contact_t = np.array(Contact_t)
-Contact_av = np.average(Contact_t, axis = 0)
-np.savetxt("./Lipid_contact_VJ/Lipid_contact.csv", Contact_t)
+ContactC_t = np.array(ContactC_t)
+ContactP_t = np.array(ContactP_t)
+ContactC_av, ContactP_av  = np.average(ContactC_t, axis = 0), np.average(ContactP_t, axis = 0)
+np.savetxt("./Lipid_contact_VJ/Lipid_Ccontact.csv", ContactC_t)
+np.savetxt("./Lipid_contact_VJ/Lipid_Pcontact.csv", ContactP_t)
 
 for i in Protein.residues.resids :
-    Protein.select_atoms('resid ' + str(i)).bfactors = [Contact_av[i-1]] * len(Protein.select_atoms('resid ' + str(i)))
+    Protein.select_atoms('resid ' + str(i)).bfactors = [ContactC_av[i-1]] * len(Protein.select_atoms('resid ' + str(i)))
 #print BackBone.bfactors to PDB file
-Protein.write('./Lipid_contact_VJ/Lipid_contact.pdb')
+Protein.write('./Lipid_contact_VJ/Lipid_contactC.pdb')
+
+for i in Protein.residues.resids :
+    Protein.select_atoms('resid ' + str(i)).bfactors = [ContactP_av[i-1]] * len(Protein.select_atoms('resid ' + str(i)))
+#print BackBone.bfactors to PDB file
+Protein.write('./Lipid_contact_VJ/Lipid_contactP.pdb')
 
 f = open('./Lipid_contact_VJ/log' + '.dat', 'w')
 f.write("Lipid-contact analyis was done with following parameter" + str(args) + "\n Output are Lipid_contact.pdb and Lipid_contact.csv")
